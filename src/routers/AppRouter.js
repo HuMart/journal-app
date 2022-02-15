9604b47ebf98669f31143fd1e08709f7c8404703
Hugo Martinez <hugommarting@gmail.com>
2022-02-15 17:12:46 -0500

    Styling login screen with sass

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { 
     BrowserRouter as Router,
+    Redirect,
     Route,
     Switch, 
 } from "react-router-dom";
@@ -22,6 +23,7 @@ export const AppRouter = () => {
                     path="/" 
                     component={JournalScreen} 
                 />
+                <Redirect to="/auth/login" />
             </Switch>
         </div>
     </Router>
--- src/routers/AuthRouter.js
+++ src/routers/AuthRouter.js
@@ -1,20 +1,28 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 import { LoginScreen } from '../components/auth/LoginScreen'
 import { RegisterScreen } from '../components/auth/RegisterScreen'
 
 
 export const AuthRouter = () => {
   return (
-      <div>
-          <Switch>
+      <div className='auth__main'>
+          <div className='auth__box-container'>
+            <Switch>
                 <Route
                     exact 
                     path="/auth/login" 
                     component={LoginScreen} 
                 />
-                <Route path={"/auth/register"} component={RegisterScreen} />
-          </Switch>
+                <Route 
+                    exact
+                    path={"/auth/register"} 
+                    component={RegisterScreen} 
+                />
+                <Redirect to="/auth/login" />
+            </Switch>
+
+          </div>
       </div>
       
   )
