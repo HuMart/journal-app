efc257a76690f59763299ff459a3af3534b5d5a2
Hugo Martinez <hugommarting@gmail.com>
2022-02-22 13:37:25 -0500

    working on the antries component

--- src/components/journal/Sidebar.js
+++ src/components/journal/Sidebar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { JournalEntries } from './JournalEntries'
 
 export const Sidebar = () => {
   return (
@@ -16,9 +17,10 @@ export const Sidebar = () => {
         </div>
 
         <div className='journal__new-entry'>
-            <i className='far fa-calendar-plus fa-5x mt-5'></i>
-            <p>New Entry</p>
+            <i className='far fa-calendar-plus fa-5x'></i>
+            <p className='mt-5'>New Entry</p>
         </div>
+        <JournalEntries />
     </aside>
  )
 }
