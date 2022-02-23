ca8e02298b0343488999bac2770139c8a502eb75
Hugo Martinez <hugommarting@gmail.com>
2022-02-23 14:23:41 -0500

    working on notes

---
+++ src/components/notes/NotesAppBar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const NotesAppBar = () => {
+  return (
+    <div className='notes__appbar'>
+        <span>February 28 2022</span>
+
+        <div>
+            <button className='btn'>
+                Picture
+            </button>
+
+            <button className='btn'>
+                Save
+            </button>
+
+        </div>
+    </div>
+  )
+}
