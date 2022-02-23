7682545449a344b3e556b8218d86480cb4986b4e
Hugo Martinez <hugommarting@gmail.com>
2022-02-23 11:41:18 -0500

    working on the antries component

--- src/components/journal/JournalEnntry.js
+++ src/components/journal/JournalEnntry.js
@@ -2,6 +2,27 @@ import React from 'react'
 
 export const JournalEnntry = () => {
   return (
-    <div>JournalEnntry</div>
+    <div className='journal__entry'>
+      <div 
+        className='journal__entry-picture'
+        style={{
+          backgroundSize: 'cover',
+          backgroundImage: 'url(https://www.bignews24.it/wp-content/uploads/2022/02/george-russell-mercedes-w13-1-VvTedh.jpeg)'
+        }}
+      >
+      </div>
+      <div className='journal__entry-body'>
+        <p className='Journal__entry-title'>
+          A new season start
+        </p>
+        <p className='Journal__entry-content'>
+          Irure nisi mollit quis cupidatat non nulla id esse velit aute irure ea minim.
+        </p>
+      </div>
+      <div className='journal__entry-date-box'>
+        <span>Wednesday</span>
+        <h4>23</h4>
+      </div>
+    </div>
   )
 }
